ddb1ffd08f28ea99fbe910c7f05083ddf97236be
mohamadkurd <hsemohamad385@gmail.com>
2024-04-21 11:14:10 +0330

    added component

--- data/data.js
+++ data/data.js
@@ -1,4 +1,4 @@
-export const character = {
+export const characters = {
   id: 1,
   name: "Rick Sanchez",
   status: "Dead",
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-
+import { characters } from "../data/data";
 import "./App.css";
+import characterDetail from "./componenet/CharacterDetail";
+import CharacterList from "./componenet/CharacterList";
+import NavBar from "./componenet/NavBar";
 
 function App(){
-  return <h1>03: Rick and Morty starter file</h1>
+  return (
+    <div className="app">
+      <NavBar/>
+      <div className="main">
+        <CharacterList characters={characters}/>
+        <characterDetail/>
+      </div>
+    </div>
+    
+  )
 }
