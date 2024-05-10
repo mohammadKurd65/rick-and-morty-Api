1406bb4b4746266285e624c5433e257ec969110c
mohamadkurd <hsemohamad385@gmail.com>
2024-05-10 19:02:20 +0330

    add useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import {characters} from '../data/data'
 import "./App.css";
 import characterDetail from "./componenet/CharacterDetail";
@@ -5,11 +6,20 @@ import CharacterList from "./componenet/CharacterList";
 import NavBar from "./componenet/NavBar";
 
 function App(){
-  const[characters, setCharacters] = useState(characters)
+  const[characters, setCharacters] = useState([])
+
+  useEffect(()=>{
+    async function feachData(){
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.jason();
+      setCharacters(data.results.slice(0, 5))
+    }
+    feachData();
+  } ,[]);
   return (
     <div className="app">
       <NavBar numOfResult={characters.length}/>
-      <Main characters={characters}>
+      <Main>
       <CharacterList characters={characters}/>
         <characterDetail/>
         <Main/>
